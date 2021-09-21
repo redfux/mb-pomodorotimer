c6dca3e7b8381968fa8c321667bacb88432afa31
redfux <57814449+redfux@users.noreply.github.com>
2021-09-21 23:47:30 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,12 @@ let timer = 0
 timer = 0
 basic.forever(function () {
     timer += 1
-    led.plotBarGraph(
-    timer,
-    25
-    )
-    if (timer >= 25) {
+    if (timer <= 25) {
+        led.plotBarGraph(
+        timer,
+        25
+        )
+    } else {
         basic.showIcon(IconNames.Yes)
     }
     basic.pause(60000)
