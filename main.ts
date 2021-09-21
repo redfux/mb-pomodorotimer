a9be779a6194e8c515bbf61a63b222e711e3b3bd
redfux <57814449+redfux@users.noreply.github.com>
2021-09-21 15:05:36 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+input.onButtonPressed(Button.A, function () {
+    timer = 0
+})
+let timer = 0
+timer = 0
+basic.forever(function () {
+    timer += 1
+    led.plotBarGraph(
+    0,
+    timer
+    )
+    if (timer >= 25) {
+        basic.showIcon(IconNames.Yes)
+    }
+    basic.pause(60000)
+})
